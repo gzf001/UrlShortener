f800883c211dbab8556a9a08d72cc971c6592cf6
gzf001 <gzf001@gmail.com>
2023-02-15 20:14:57 -0300

    Incorporación swagger

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-
 const urlShortener = require("./src/routes/urlShortener.route");
 
+const path = require("path");
+
+const swaggerUI = require("swagger-ui-express");
+
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const doc = require("./swagger.json");
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use("/doc", swaggerUI.serve, swaggerUI.setup(doc));
 app.use("/", urlShortener);
 
 app.listen(port, () => {
